56778aa867ea1e295d3e15e4045d7d7620d82a20
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-07-18 13:34:53 -0700

    baseline files and folders added 7/18/24

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const PORT = 3000;
+
+// serve static files from 'public' folder in root directory
+app.use(express.static('public'));
+
+// parse URL-encoded data submitted by forms (makes accessible through req.body)
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+// any route not defined is 404'ed
+app.use('*', (req, res) => {
+    return res.status(404).send('404: Page not found- you silly goose');
+  });
+  
+  // Global Error Handler
+  app.use((error, req, res, next) => {
+    const defaultMessage = 'Uh-oh SpaghettiOs (something went wrong)!';
+    const message = error.message || defaultMessage;
+    console.log(message);
+    return res.status(500).send(message);
+  });
+  
+  // start server on port 3000
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
