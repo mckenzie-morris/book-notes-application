58cf38d17c334c96d925fe7764276b20198ca18e
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-08-29 16:06:19 -0700

    theme setting reload issue fixed and comments added 8/29/24

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 
 const PORT = 3000;
 
+// initialize query data and cache
 let queryResults = undefined;
 const queryCache = {};
 
@@ -19,39 +20,55 @@ app.use(express.static("dist"));
 // parse URL-encoded data submitted by forms (makes accessible through req.body)
 app.use(express.urlencoded({ extended: true }));
 
+// render homepage at root route
 app.get("/", (req, res) => {
   return res.render("index.ejs", { queryResults: queryResults });
 });
 
+// render homepage + query results at '/search' endpoint
 app.post("/search", async (req, res) => {
   console.log(req.body);
-  console.log("server-side reached");
-  const lastTheme = req.body.currentThemeSetting
+  // pull the current theme from input with 'type='hidden'
+  const lastTheme = req.body.currentThemeSetting;
+  // pull query from input, and format the data (lowercase and no white space)
   const queryParams = req.body.query.toLowerCase().trim();
+  /* if the most recent query has been cached, render the homepage with the 
+  corresponding results */
   if (queryParams in queryCache) {
-    console.log("found in cache! ", queryCache);
+    console.log(queryCache, "\nFOUND in cache!");
+    // pull the results of the previously made query from the cache
     queryResults = queryCache[queryParams];
+    // render the homepage with the cached query key-value pair
     return res.render("index.ejs", {
       queryResults: queryResults,
       lastQuery: queryParams,
-      lastTheme: lastTheme
+      lastTheme: lastTheme,
     });
   } else {
+    /* if the most recent query has NOT been cached, make API call with the
+  most recent query as the URL query parameter, then add the query as a
+  key to the cache and the response data as the value */
+    // make async call to API with necessary Axios Request Config object
     try {
       console.log("NOT found in cache!");
-      const response = await axios({
-        method: "GET",
-        url: "https://api.openbrewerydb.org/v1/breweries/autocomplete",
-        params: { query: queryParams },
-      });
-      // console.log(JSON.stringify(response.data));
+      const response = await axios(
+        // Request Config object
+        {
+          method: "GET",
+          url: "https://api.openbrewerydb.org/v1/breweries/autocomplete",
+          params: { query: queryParams },
+        },
+      );
+      // add the most recent query and its response data to the cache
       queryCache[queryParams] = response.data;
+      // render the homepage with the (now) cached query key-value pair
       return res.render("index.ejs", {
         queryResults: response.data,
         lastQuery: queryParams,
-        lastTheme: lastTheme
+        lastTheme: lastTheme,
       });
     } catch (error) {
+      // if error encountered during API call, log it to the console
       console.log(error);
     }
   }
@@ -76,14 +93,3 @@ app.listen(PORT, () => {
 });
 
 // USE https://www.openbrewerydb.org/documentation/
-
-/* 
-
-**DEBOUNCE** User begins typing in search bar --> when user stops typing for (?) 1000ms, call API -->
-if user begins typing again within the 1000ms timeframe, reset timer --> 
-**CACHE** --> if timer has reached 0, send API call and cache search term and corresponding results -->
-before sending another API call (if time has elapsed) check if search term is included in cache keys -->
-if search term is a cache key, return value (search results) and do not initiate debounce again unless 
-search term has changed --> if search term is not a cache key, make API call
-
-*/
--- src/main.js
+++ src/main.js
@@ -8,42 +8,55 @@ $(() => {
   // initialize the html to light theme
   $("html").addClass("light");
   $("#themeToggleContainer").addClass("light");
+
+// dynamically-generated data sent from server to data-* attributes on '#user_input'
+// will be an empty object literal before first query is made
+const serverData = $("#user_input").data();
+console.log(serverData);
+// current theme ('light' or 'dark')
+const currentTheme = $("html")[0].className;
+
+
+/* if a query has been made, and the server's API call has successfuly returned data
+and rendered the homepage, display the data, focus the input field, and set the
+theme to the setting prior to the API call and page render */
+if (Object.keys(serverData).length) {
+  // list of returned breweries from the API call
+  const queryResults = $("#user_input").data().query_results;
+  // the query that was sent on the API call
+  const lastQuery = $("#user_input").data().last_query;
+  // the theme setting prior to the API call
+  const lastTheme = $("#user_input").data().last_theme;
+  /* if the current theme does not match the theme setting prior to the API call, 
+  switch the theme */
+  if (currentTheme !== lastTheme) {
+    $("#themeToggle").trigger("click");
+  }
+  // focus the input field
+  $("#user_input").trigger("focus");
+  // populate the input with the query sent on the API call
+  $("#user_input").val(lastQuery);
+}
 });
 
+/* if the '#themeToggle' button is clicked, swap theme 
+from 'light' to 'dark', or 'dark' to 'light' */
 $("#themeToggle").on("click", () => {
-  $("html").toggleClass("light dark");
-  $("#themeToggleContainer").toggleClass("light dark");
-  if ($("#themeIcon").attr("src") === "light-icon.svg") {
+  if ($("html")[0].className === "light") {
+    $("html").toggleClass("light dark");
+    $("#themeToggleContainer").toggleClass("light dark");
     $("#themeIcon").attr("src", "dark-icon.svg");
-  } else {
+  } else if ($("html")[0].className === "dark") {
+    $("html").toggleClass("dark light");
+    $("#themeToggleContainer").toggleClass("dark light");
     $("#themeIcon").attr("src", "light-icon.svg");
   }
 });
 
-//////////////////////////////////////////////////////////
-
-const serverData = $("#user_input").data();
-
-console.log(serverData);
-
-if (serverData.last_query) {
-  const queryResults = $("#user_input").data().query_results;
-  const lastQuery = $("#user_input").data().last_query;
-
-///////////////////*****YOU ARE HERE*****///////////////////
-
-  const lastTheme = $("#user_input").data().last_theme
-  $("html").toggleClass(`${$('html')[0].className} ${lastTheme}`)
-
-///////////////////*****YOU ARE HERE*****///////////////////
-
-  $("#user_input").trigger("focus");
-  $("#user_input").val(lastQuery);
-}
-
+/* on query submission, attach the current theme setting ('light' or 'dark') to
+the input field with attribute 'type=hidden' */
 $("#search").on("submit", () => {
-  console.log("submitted!");
-  $('#currentThemeSetting').val($('html')[0].className)
+  $("#currentThemeSetting").val($("html")[0].className);
 });
 
 // triggerd each time input field changes
@@ -53,7 +66,7 @@ $("#user_input").on("input", () => {
     const debouncer = setTimeout(() => {
       // console.log('Timeout reached, input submitted: ', $("#user_input").val())
       $("#search").trigger("submit");
-    }, 1500);
+    }, 1000);
     /* if user begins typing before necessary time has elapsed, reset timer and 
   eliminate pending function execution */
     $("#user_input").on("input", () => {
@@ -62,5 +75,3 @@ $("#user_input").on("input", () => {
     });
   }
 });
-
-//////////////////////////////////////////////////////////
