23992e134c3ac37f0205c6562453a5a4f2e2c9a8
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-07 12:44:29 -0700

    Webpack for notes endpoint re-established 10/7/24

--- src/notes.js
+++ src/notes.js
@@ -1,20 +1,24 @@
-import './styles.css';
-import lightModeIcon from './assets/light-icon.svg';
-import darkModeIcon from './assets/dark-icon.svg';
-import $ from 'jquery';
+import "./styles.css";
+import lightModeIcon from "./assets/light-icon.svg";
+import darkModeIcon from "./assets/dark-icon.svg";
+import $ from "jquery";
 
 // on document ready
 $(() => {
   // initialize the html to dark theme
-  $('html').addClass('light');
-  $('#themeToggleContainer').addClass('light');
+  $("html").addClass("light");
+  $("#themeToggleContainer").addClass("light");
 });
 
-$('#themeToggle').on('click', () => {
-  $('html').toggleClass('light dark');
-  $('#themeToggleContainer').toggleClass('light dark');
-  if( $('#themeIcon').attr('src') === 'light-icon.svg' ) {
-    $('#themeIcon').attr('src', 'dark-icon.svg')
+$("#themeToggle").on("click", () => {
+  console.log("clicked");
+  if ($("html")[0].className === "light") {
+    $("html").toggleClass("light dark");
+    $("#themeToggleContainer").toggleClass("light dark");
+    $("#themeIcon").attr("src", "/dark-icon.svg");
+  } else if ($("html")[0].className === "dark") {
+    $("html").toggleClass("dark light");
+    $("#themeToggleContainer").toggleClass("dark light");
+    $("#themeIcon").attr("src", "/light-icon.svg");
   }
-  else {$('#themeIcon').attr('src', 'light-icon.svg')}
 });
