4aa8e7eb202c755a7ceacd757bf1cba58dea5aa3
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-08 16:22:37 -0700

    Accordion almost complete 10/8/24

--- src/notes.js
+++ src/notes.js
@@ -1,7 +1,8 @@
 import "./styles.css";
 import lightModeIcon from "./assets/light-icon.svg";
 import darkModeIcon from "./assets/dark-icon.svg";
-import chevron from "./assets/chevron.svg"
+import lightChevron from "./assets/chevron-light.svg"
+import darkChevron from "./assets/chevron-dark.svg"
 import $ from "jquery";
 
 // on document ready
@@ -12,14 +13,15 @@ $(() => {
 });
 
 $("#themeToggle").on("click", () => {
-  console.log("clicked");
   if ($("html")[0].className === "light") {
     $("html").toggleClass("light dark");
     $("#themeToggleContainer").toggleClass("light dark");
     $("#themeIcon").attr("src", "/dark-icon.svg");
+    $('.accordionChevron').attr("src", "/chevron-dark.svg")
   } else if ($("html")[0].className === "dark") {
     $("html").toggleClass("dark light");
     $("#themeToggleContainer").toggleClass("dark light");
     $("#themeIcon").attr("src", "/light-icon.svg");
+    $('.accordionChevron').attr("src", "/chevron-light.svg")
   }
 });
